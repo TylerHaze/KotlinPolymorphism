40ede8c48fda5ab17b978a9cdf72f086308ab907
TylerHaze <tylerhazleton@gmail.com>
2018-03-04 23:23:42 -0500

    Updated carparts file

--- src/Car.kt
+++ src/Car.kt
@@ -1,15 +1,14 @@
+
 class Car constructor(fuelTank: Int, numCylinders: Int, condition: Int){
 
     var engine = Engine(numCylinders,condition)
     var fuel = FuelTank(fuelTank,condition)
-    private val parts : ArrayList<CarPart> = arrayListOf(engine, fuel)
-
-
+    var parts = mutableListOf(engine, fuel)
     fun run(){
         println("vroom")
         for(i in parts.indices){
-            parts.get(i).function()
-            parts.get(i).status()
+            parts[i].function()
+            parts[i].status()
             println("/////////////////")
         }
     }
